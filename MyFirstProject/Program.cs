0e93a9534017bde318aff075e2df7cdca4ae2431
Atuma David <atumadavid35@gmail.com>
2024-11-12 13:14:32 +0100

    Add project files.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise - Storing User Data
+            // one
+            var name = "David";
+            var number = "0123456789";
+            var age = 28;
+
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(age);
+
+            // two
+            int num1 = 10;
+            int num2 = 2;
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+
+            Console.ReadLine();
+        }
+    }
+}

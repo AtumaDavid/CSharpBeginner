1911cbd63015c06ffe57b07ba5a2bf41501bfb4d
Atuma David <atumadavid35@gmail.com>
2024-11-12 16:44:00 +0100

    exeercise - password match

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -105,6 +105,35 @@ namespace MyFirstProject
              * -- If they are empty, print "please enter a password"
              */
 
+            Console.Write("Enter Password: ");
+            string password = Console.ReadLine();
+            
+            Console.Write("Enter Password again: ");
+            string passwordC = Console.ReadLine();
+
+            if (!password.Equals(string.Empty) ) 
+            {
+                if (!passwordC.Equals(string.Empty))
+                {
+                    if (password.Equals(passwordC))
+                    {
+                        Console.WriteLine("passwords match");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Passwords do not match");
+                    }
+                } 
+                else
+                {
+                    Console.WriteLine("Please enter a password confirmation");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a password.");
+            }
+
             Console.ReadLine();
         }
     }
